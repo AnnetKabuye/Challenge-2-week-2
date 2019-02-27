f7fd0e96a1b9ee64bd317540047e34652685961a
Annet Kabuye <ananungi@gmail.com>
2019-02-27 23:55:40 +0300

    first commit

---
+++ vowel_counter.py
@@ -0,0 +1,11 @@
+
+def vowel_counter():
+    mystr = input("input a  string of  your choice")
+    count=0
+    vowels=['a','e','i','o','u','A','E','I','O','U']
+    for vowels in mystr:
+        count +=1
+        new_vowels =[]
+        v = new_vowels.append(vowels)
+        print (v, count)
+vowel_counter()
