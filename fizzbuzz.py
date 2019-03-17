81f4a750ba9bc402fba51c09b7b38d590d24229e
Annet Kabuye <ananungi@gmail.com>
2019-03-17 23:12:33 +0300

    First commit

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(a, b):
+    pass
+    try:
+        combined_length= len(a)+len(b)
+        if combined_length%3==0:
+            return "fizz"
+        elif combined_length%5==0:
+            return "buzz"
+        elif combined_length%3==0 and combined_length%5==0:
+            return "fizzbuzz"
+        else:
+            return combined_length
+            
+    except TypeError:
+        return "Invalid input"   
