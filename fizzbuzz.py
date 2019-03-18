dc35374d0b01bdcdaa727c3b62e6fe078a0a6cee
Annet Kabuye <ananungi@gmail.com>
2019-03-18 16:50:41 +0300

    second  commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,12 +2,12 @@ def fizzbuzz(a, b):
     pass
     try:
         combined_length= len(a)+len(b)
-        if combined_length%3==0:
-            return "fizz"
+        if combined_length%3==0 and combined_length%5==0:
+            return "fizzbuzz"
         elif combined_length%5==0:
             return "buzz"
-        elif combined_length%3==0 and combined_length%5==0:
-            return "fizzbuzz"
+        elif combined_length%3==0:
+            return "fizz"
         else:
             return combined_length
             
